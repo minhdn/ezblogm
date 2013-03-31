17d5f800c6fc32439834a5f7cc813f23a486210c
Minh D <minhdn@outlook.com>
2013-03-31 11:10:39 -0700

    3-31-13-1110am

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ezblogm::Application.config.secret_token = 'f14e3d0861fcb3f4f641699e1628c4198dc70f4e533c33939fc810c50d76c79ca7b750b8b51a7aaebd3b5aa48f0829536705a11da4b1fa4ac1b2df11feb0ea09'
